8731ecebe9f86237401404de643da89dd6f51fad
John Gruber <j.gruber@f5.com>
2023-01-25 11:29:30 -0600

    ignore images where the image_id is null in catalog

--- bigip_image_selector.py
+++ bigip_image_selector.py
@@ -6,8 +6,10 @@ import urllib.request
 import difflib
 
 ALLOWED_TMOS_TYPES = ['all', 'ltm']
-PUBLIC_REGIONS = ['us-south', 'us-east', 'eu-gb',
-                  'eu-de', 'jp-tok', 'jp-osa', 'au-syd', 'ca-tor', 'br-sao']
+PUBLIC_REGIONS = [
+    'us-south', 'us-east', 'eu-gb', 'eu-de', 'jp-tok', 'jp-osa', 'au-syd',
+    'ca-tor', 'br-sao'
+]
 
 
 def get_public_images(region):
@@ -19,8 +21,8 @@ def get_public_images(region):
         response = urllib.request.urlopen(catalog_url)
         return json.load(response)
     except Exception as ex:
-        sys.stderr.write(
-            'Can not fetch F5 image catalog at %s: %s' % (catalog_url, ex))
+        sys.stderr.write('Can not fetch F5 image catalog at %s: %s' %
+                         (catalog_url, ex))
         sys.exit(1)
 
 
@@ -29,10 +31,10 @@ def longest_substr(type, catalog_image_name, version_prefix):
         return 0
     if catalog_image_name.find(version_prefix) < 0:
         return 0
-    seqMatch = difflib.SequenceMatcher(
-        None, catalog_image_name, version_prefix)
-    match = seqMatch.find_longest_match(
-        0, len(catalog_image_name), 0, len(version_prefix))
+    seqMatch = difflib.SequenceMatcher(None, catalog_image_name,
+                                       version_prefix)
+    match = seqMatch.find_longest_match(0, len(catalog_image_name), 0,
+                                        len(version_prefix))
     return match.size
 
 
@@ -43,8 +45,9 @@ def get_image(region, type, version):
     image_name = None
     image_id = None
     for image in image_catalog[region]:
-        match_length = longest_substr(
-            type, image['image_name'], version)
+        if not image['image_id']:
+            continue
+        match_length = longest_substr(type, image['image_name'], version)
         if match_length > 0 and match_length >= max_match:
             max_match = match_length
             image_url = image['image_sql_url']
@@ -57,31 +60,36 @@ def main():
     jsondata = json.loads(sys.stdin.read())
     if 'type' not in jsondata:
         sys.stderr.write(
-            'type, region, verion_prefix inputs require to query public f5 images')
+            'type, region, verion_prefix inputs require to query public f5 images'
+        )
         sys.exit(1)
     if 'region' not in jsondata:
         sys.stderr.write(
-            'type, region, verion_prefix inputs require to query public f5 images')
+            'type, region, verion_prefix inputs require to query public f5 images'
+        )
         sys.exit(1)
     if 'version_prefix' not in jsondata:
         sys.stderr.write(
-            'type, region, verion_prefix inputs require to query public f5 images')
+            'type, region, verion_prefix inputs require to query public f5 images'
+        )
         sys.exit(1)
     tmos_type = jsondata['type'].lower()
     if tmos_type not in ALLOWED_TMOS_TYPES:
-        sys.stderr.write('TMOS type must be in: %s' %
-                         ALLOWED_TMOS_TYPES)
+        sys.stderr.write('TMOS type must be in: %s' % ALLOWED_TMOS_TYPES)
         sys.exit(1)
     tmos_version_match = jsondata['version_prefix'].lower().replace('.', '-')
     region = jsondata['region'].lower()
     if region not in PUBLIC_REGIONS:
         sys.stderr.write(
-            'public f5 images are not supported in region: %s. Use a custom VPC image.' % region)
+            'public f5 images are not supported in region: %s. Use a custom VPC image.'
+            % region)
         sys.exit(1)
-    (image_url, image_name, image_id) = get_image(region, tmos_type, tmos_version_match)
+    (image_url, image_name, image_id) = get_image(region, tmos_type,
+                                                  tmos_version_match)
     if not image_url:
         sys.stderr.write(
-            'No image in the public image catalog matched version %s' % tmos_version_match)
+            'No image in the public image catalog matched version %s' %
+            tmos_version_match)
         sys.exit(1)
     jsondata['image_sql_url'] = image_url
     jsondata['image_name'] = image_name
